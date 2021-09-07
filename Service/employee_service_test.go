a365e4b87122c37070b787582876d2875c29957d
JevaPrahayusma <jeva@gospurr.com>
2021-09-08 05:58:20 +0700

    Unit Testing CRUD Done

--- Service/employee_service_test.go
+++ Service/employee_service_test.go
@@ -1,6 +1,7 @@
 package Service
 
 import (
+	"errors"
 	"testing"
 
 	"github.com/caklucky/crud-unit-test/models"
@@ -69,4 +70,71 @@ func TestEmployeeService_TambahPegawaiSuccess(t *testing.T) {
 	assert.NotNil(t, result)
 	assert.NotEmpty(t, result)
 	assert.Equal(t, ekspetasi.ID, result.ID)
+}
+func TestEmployeeService_TambahPegawaiNotSuccess(t *testing.T) {
+	repo := new(repository.EmployeeRepositoryMock)
+	service := NewEmployeeService(repo)
+	ekspetasi := models.Employee{
+		Name:   "sukijan",
+		ID:     "1",
+		Gender: "M",
+		Phone:  "08112002211",
+	}
+	repo.Mock.On("Insert", mock.AnythingOfType("models.Employee")).Return(nil).Once()
+	repo.Mock.On("FindById", mock.AnythingOfType("string")).Return(models.Employee{}, nil).Once()
+
+	result, err := service.TambahPegawai(ekspetasi)
+	assert.NoError(t, err)
+	assert.Empty(t, result)
+}
+func TestEmployeeService_UpdatePegawaiNotSuccess(t *testing.T) {
+	repo := new(repository.EmployeeRepositoryMock)
+	service := NewEmployeeService(repo)
+	ekspetasi := models.Employee{
+		Name:   "sukijan",
+		ID:     "1",
+		Gender: "M",
+		Phone:  "08112002211",
+	}
+	error := errors.New("id not found ")
+	repo.Mock.On("FindById", ekspetasi.ID).Return(models.Employee{}, nil).Once()
+
+	err := service.UpdatePegawai(ekspetasi)
+	assert.Error(t, err)
+	assert.EqualError(t, error, err.Error())
+
+}
+func TestEmployeeService_DeletePegawaiNotSuccess(t *testing.T) {
+	repo := new(repository.EmployeeRepositoryMock)
+	service := NewEmployeeService(repo)
+	ekspetasi := models.Employee{
+		Name:   "sukijan",
+		ID:     "1",
+		Gender: "M",
+		Phone:  "08112002211",
+	}
+	repo.Mock.On("FindById", ekspetasi.ID).Return(ekspetasi, nil).Once()
+	repo.Mock.On("Update", mock.AnythingOfType("models.Employee")).Return(nil).Once()
+
+	err := service.UpdatePegawai(ekspetasi)
+	assert.NoError(t, err)
+}
+func TestEmployeeService_HapusPegawaiNotFound(t *testing.T) {
+	repo := new(repository.EmployeeRepositoryMock)
+	service := NewEmployeeService(repo)
+	error := errors.New("id not found ")
+	repo.Mock.On("Delete", "1").Return(error).Once()
+
+	err := service.HapusPegawai("1")
+	assert.Error(t, err)
+	assert.EqualError(t, error, err.Error())
+}
+func TestEmployeeService_HapusPegawaiSuccess(t *testing.T) {
+	repo := new(repository.EmployeeRepositoryMock)
+	service := NewEmployeeService(repo)
+	repo.Mock.On("Delete", "1").Return(nil).Once()
+
+	err := service.HapusPegawai("1")
+	assert.NoError(t, err)
+
 }
